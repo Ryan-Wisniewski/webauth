d4c19f14f2aa6e5b4b3c464a9d164ec6e6fd72ee
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-09-16 18:05:32 -0400

    cool everything is working!

--- router/userRouter.js
+++ router/userRouter.js
@@ -20,20 +20,35 @@ router.get('/', test.idCheck, (req, res) => {
 
 router.post('/register', (req, res) => {
     let { username, password } = req.body
-
+    //its magic
     const hash = bcryptjs.hashSync(password, 10)
+
     userDb.insert({username, password: hash})
         .then(newUser => {
             res.status(201).json(newUser)
         })
         .catch(err => {
             console.log(err)
-          res.status(500).json({ error: "There was an oopsies"});
-        });
+          res.status(500).json({ error: "There was an oopsies"})
+        })
 });
 
 router.post('/login', (req, res) => {
-    
+    let { username, password } = req.body
+
+    userDb.getBy({ username })
+    .first()
+    .then(user => {
+        if (user && bcryptjs.compareSync(password, user.password)){
+            res.status(200).json({ message: `Yayyyy you passed the hashtest: ${user.username}`})
+        } else {
+            res.status(401).status({ message: 'Hah TRY AGAIN'})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+      res.status(500).json({ error: "There was an oopsies"})
+    })
 });
 
 module.exports = router
